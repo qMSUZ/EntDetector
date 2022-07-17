9bfabf6389cd3521a7992642a1a52543a55d0646
qMSUZ <m.sawerwain@issi.uz.zgora.pl>
2022-07-17 23:09:55 +0200

    update with full function "entroy_by"

--- CC-for-Smqs-Examples/total-ent-and-deficit.py
+++ CC-for-Smqs-Examples/total-ent-and-deficit.py
@@ -53,6 +53,146 @@ import time
 
 from entdetector import *
 
+def entropy_by_paritition_division_for_given_partition_list(queue, q, lp, nqubits, verbose ):
+    #s = q.size
+    s = nqubits
+    # generation of all two partitite divisions of given
+    # set which is build from the quantum register q
+    total_entropy_val = 0.0
+    total_negativity_val = 0.0
+    entropy_val = 0.0
+    res = [ ]
+    idxtoremove = [ ]
+#    k = [0] * s
+#    M = [0] * s
+#    p = 2
+#    partititon_p_initialize_first(k, M, p)
+#    lp = []
+#    lp = lp + [make_partititon_as_list(k)]
+#    while partition_p_next(k, M, p):
+#        lp = lp + [make_partititon_as_list(k)]
+    for i in lp:
+            if verbose==1 or verbose==2:
+                    print(i[0], i[1])
+            mxv=2**len(i[0])
+            myv=2**len(i[1])
+            if verbose==1:
+                    print(mxv,"x",myv)
+            #m=qcs.Matrix(mxv, myv)
+            m  = np.zeros((mxv, myv), dtype=complex)
+            #mt=qcs.Matrix(mxv, myv)
+            mt = np.zeros((mxv, myv), dtype=complex)
+            for x in range(0,mxv):
+                    for y in range(0, myv):
+                            xstr=bin(x)[2:]
+                            ystr=bin(y)[2:]
+                            xstr='0'*(len(i[0])-len(xstr)) + xstr
+                            ystr='0'*(len(i[1])-len(ystr)) + ystr
+                            cstr=[0]*s
+                            for xidx in range(0, len(xstr)):
+                                    idx = i[0][xidx]
+                                    cstr[idx]=xstr[xidx]
+                            for yidx in range(0, len(ystr)):
+                                    idx = i[1][yidx]
+                                    cstr[idx]=ystr[yidx]
+                            cidx=""
+                            for c in cstr:
+                                    cidx=cidx+c
+                            dcidx=bin2dec(cidx)
+                            dxidx=bin2dec(xstr)
+                            dyidx=bin2dec(ystr)
+                            if verbose==1:
+                                    print("D("+xstr+","+ystr+")","D(",dxidx,dyidx,") C",dcidx,cidx,cstr)
+                            #m.AtDirect(dxidx,dyidx, q.GetVecStateN(dcidx).Re(), q.GetVecStateN(dcidx).Im())
+                            m[dxidx,dyidx] = q[dcidx]
+                            #mt.AtDirect(dxidx,dyidx, q.GetVecStateN(dcidx).Re(), q.GetVecStateN(dcidx).Im())
+                            mt[dxidx,dyidx] = q[dcidx]
+            if verbose==1:
+                    #m.PrMatlab()
+                    print("m matrix")
+                    print(m)
+            #mf=m.Calc_D_dot_DT() # D * D'
+            mf = m @ m.transpose()
+            #sd=mf.SpectralDecomposition()
+            #sd.eigenvalues.Chop()
+            #ev_count=sd.eigenvalues.NonZeros()
+            (ev,evec)=eigen_decomposition(mf)
+            ev=chop(ev)
+            ev_count = np.count_nonzero(ev)
+            if (ev_count > 1):
+                entropy_val=0.0
+                negativity_val=0.0
+                for ee in ev:
+                    #e=np.sqrt(ee)
+                    e=ee
+                    if e != 0.0:
+                        entropy_val = entropy_val + (e ** 2) * np.log((e ** 2))
+                        #entropy_val = entropy_val + e * np.log2(e)
+                        #entropy_val = entropy_val + e * np.log10(e)
+                        negativity_val=negativity_val+e
+                total_negativity_val += negativity_val
+                total_entropy_val += -entropy_val
+            
+            if verbose==1 or verbose==2:
+                    print("non zero:", ev_count)
+                    print("ev=",ev)
+            if (ev_count==1) and (len(i[0])==1):
+                idxtoremove=idxtoremove+i[0]
+                
+            if (ev_count==1) and (len(i[1])==1):
+                idxtoremove=idxtoremove+i[1]
+                
+            res=res + [[ev_count, [i[0], i[1]]]]
+            if verbose==1 or verbose==2:
+                print()
+    queue.put( (res, idxtoremove, total_entropy_val, 0.5*((total_negativity_val ** 2)-1.0) ) )
+
+def entropy_by_paritition_division_parallel_v0( q, nqubits, ntasks, verbose ):
+    #s = q.size
+    s = nqubits
+    # generation of all two partitite divisions of given
+    # set which is build from the quantum register q
+    ret_total_entropy_val = 0.0
+    ret_total_negativity_val = 0.0
+    entropy_val = 0.0
+    ret_res = [ ]
+    ret_idxtoremove = [ ]
+    k = [0] * s
+    M = [0] * s
+    p = 2
+
+    partititon_p_initialize_first(k, M, p)
+
+    lp = []
+    lp = lp + [make_partititon_as_list(k)]
+    while partition_p_next(k, M, p):
+        lp = lp + [make_partititon_as_list(k)]
+
+    length_of_single_part = len(lp) / float(ntasks)
+    list_of_parts = [ ]
+    num_of_elements=0
+
+    while num_of_elements < len(lp):
+        list_of_parts.append(lp[int(num_of_elements):int(num_of_elements + length_of_single_part)])
+        num_of_elements += length_of_single_part
+
+    queue = multiprocessing.Queue()
+    tasks = range(ntasks)
+    
+    for part in list_of_parts:
+        p=multiprocessing.Process(target=entropy_by_paritition_division_for_given_partition_list, args=(queue, q, part, nqubits, verbose))
+        p.start()
+        
+    for _ in tasks:
+        (r,idxtoremove,total_entropy_val,total_negativity_val) = queue.get()
+        ret_res = ret_res + r
+        ret_idxtoremove = ret_idxtoremove + idxtoremove
+        ret_total_entropy_val = ret_total_entropy_val + total_entropy_val
+        ret_total_negativity_val = ret_total_negativity_val + total_negativity_val
+
+    return ret_res, ret_idxtoremove, ret_total_entropy_val, 0.5*((ret_total_negativity_val ** 2)-1.0)
+
+
 def total_entropy_and_deficit(d, nqubits, ntasks):
     #d = 2
     #nqubits = 2
