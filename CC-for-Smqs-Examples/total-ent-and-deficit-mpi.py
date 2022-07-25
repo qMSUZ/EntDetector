d9fb977bef19bed001bc96a8d38b52b7f2315ab5
unknown <m.sawerwain@issi.uz.zgora.pl>
2022-07-25 12:33:40 +0200

    initial file for mpi version

---
+++ CC-for-Smqs-Examples/total-ent-and-deficit-mpi.py
@@ -0,0 +1,63 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+#/***************************************************************************
+# *   Copyright (C) 2021 -- 2022 by Marek Sawerwain                         *
+# *                                  <M.Sawerwain@gmail.com>                *
+# *                                  <M.Sawerwain@issi.uz.zgora.pl>         *
+# *                                                                         *
+# *                              by Joanna Wiśniewska                       *
+# *                                  <Joanna.Wisniewska@wat.edu.pl>         *
+# *                                                                         *
+# *                              by Roman Gielerak                          *
+# *                                  <R.Gielerak@issi.uz.zgora.pl>          *
+# *                                                                         *
+# * file is related to the paper titled                                     *
+# *   Classical Computer assisted  analysis  of small multi-qudit systems   *
+# *    published in IEEE Access Vol.???, No.???, pages ?????, Year          *
+# *                                                                         *
+# *   Part of the EntDetector:                                              *
+# *         https://github.com/qMSUZ/EntDetector                            *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation; either version 3 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU General Public License for more details.                          *
+# *                                                                         *
+# *   You should have received a copy of the GNU General Public License     *
+# *   along with this program; if not, write to the                         *
+# *   Free Software Foundation, Inc.,                                       *
+# *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+# ***************************************************************************/
+
+
+
+import os
+import numpy as np
+import scipy.special
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import matplotlib.colors as colors
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import cmath 
+
+#import contextlib
+
+import multiprocessing
+import time
+#import more_itertools
+
+import mpi4py
+
+from entdetector import *
+
+
+if __name__ == '__main__':
+    pass
